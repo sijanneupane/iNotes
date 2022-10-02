b349e486bab42d359b00e6fa948b96b6a815a56b
Sijan <sijan@greatbear.tech>
2022-10-02 11:19:12 +0545

    readme

--- src/components/Note Search.js
+++ src/components/Note Search.js
@@ -3,7 +3,17 @@ import React from 'react'
 const About = () => { 
     return (
         <div>
-            This is About page
+           <div className="main">
+      <h1>Note Search</h1>
+      <div className="search">
+        <TextField
+          id="outlined-basic"
+          variant="outlined"
+          fullWidth
+          label="Search"
+        />
+      </div>
+      </div>
         </div>
     )
 }
